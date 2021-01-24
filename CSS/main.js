8901e662c8a7f2be4f205b83151dc095ffd862ce
jiyong16 <p_ji_y@naver.com>
2021-01-24 22:11:22 +0900

    season_img slider add features

---
+++ CSS/main.js
@@ -0,0 +1,13 @@
+var num = 1;
+function control(direct){
+  if (direct) {
+    if (num == 3) return;
+    num++;
+  } else {
+    if (num == 1) return;
+    num--;
+}
+
+var imgTag = document.getElementById("season");
+imgTag.setAttribute("src", "CSS/comingsoon_" + num + ".jpg");
+}
